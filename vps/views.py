39625ba150d60a146bcc7a4dee1e6a314b4f3896
Harsh Verma <harshverma0362@gmail.com>
2021-05-10 22:09:43 +0530

    implimented materialize css

--- vps/views.py
+++ vps/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def home_page(request):
     return render(request, 'index.html', { 'title': "Welcome To Vimal Public School" })
+
+def contact_page(request):
+    return render(request, 'contact.html', { 'title': "Vimal Public School | Contact Us" })
