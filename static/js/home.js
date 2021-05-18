40ad42ca2fe6573c1a46a19f6ec26953a23e85a8
Sonali Singh <sonali122000@gmail.com>
2021-05-18 22:05:43 +0530

    add nav and slider code

--- static/js/home.js
+++ static/js/home.js
@@ -1,6 +1,11 @@
 document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('.sidenav');
     var instances = M.Sidenav.init(elems, {});
+
+    var elems = document.querySelectorAll('.carousel');
+    var instances = M.Carousel.init(elems, {fullWidth : true});
+
+    console.log("inside js")
   });
   // Initialize collapsible (uncomment the lines below if you use the dropdown variation)
  // var collapsibleElem = document.querySelector('.collapsible');
