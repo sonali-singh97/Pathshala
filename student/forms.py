0cf37d63a34c96a31a757fcff5c4288ddc53dd25
Harsh Verma <harshverma0362@gmail.com>
2021-07-07 14:07:17 +0530

    login ui fixed

--- student/forms.py
+++ student/forms.py
@@ -2,5 +2,5 @@ from django import forms
 from django.contrib.auth.forms import AuthenticationForm, UsernameField
 
 class LoginForm(AuthenticationForm):
-    username = UsernameField(widget=forms.TextInput(attrs={'autofocus': True, 'placeholder': 'Enter Admission Number...'}))
-    password = forms.CharField(widget=forms.PasswordInput(attrs={'placeholder': 'Enter Password...'}))
+    username = UsernameField(widget=forms.TextInput(attrs={'autofocus': True, 'placeholder': 'Enter Admission Number...', 'class': 'input-field-box'}))
+    password = forms.CharField(widget=forms.PasswordInput(attrs={'placeholder': 'Enter Password...', 'class': 'input-field-box'}))
