2463567a28d5fbbf1c704e61e9450e753e2d503c
Harsh Verma <harshverma0362@gmail.com>
2021-07-07 13:58:02 +0530

    resources section completed

--- academics/models.py
+++ academics/models.py
@@ -4,4 +4,4 @@ class Resource(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField(max_length=500)
     createdOn = models.DateField()
-    pdf = models.URLField()
+    pdf = models.FileField(upload_to='pdf')
