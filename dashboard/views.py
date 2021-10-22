d1db1c68349f02fe1ddc9119eee0a9d7457f33d0
Sonali Singh <sonali122000@gmail.com>
2021-10-22 22:35:12 +0530

    create base template for dashboard

--- dashboard/views.py
+++ dashboard/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def dashboard(request):
      return render(request, 'dashboard/home.html')
+
+def all_fees(request):
+     return render(request, 'dashboard/all_fees.html')
